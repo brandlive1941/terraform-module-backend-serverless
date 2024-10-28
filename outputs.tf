08333716b73a3b5894659d2aff61c058aa345a2e
drewmercer-bl <drew.mercer@brandlive.com>
2024-10-28 11:15:12 -0700

    feat(main.tf): adding custom error response policies

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "services" {
   description = "Local Details"
   value       = local.cloud_run_services
 }
+
+output "default_custom_error_response_policy" {
+  description = "Default custom error response policy"
+  value       = var.default_custom_error_response_policy
+}
